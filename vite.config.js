f53edae9966688c0886072398923fc2b563d062b
Suglobov Dima <suglobovdima@ya.ru>
2024-07-08 22:01:40 +0300

    /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/{repo-name}/',
+  base: process.env.NODE_ENV === 'production' ? '/lndst-react/' : '/',
+
+
 })
